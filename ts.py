9e32fa24a972ba3a28a1d95ea70bfb2fc5c7fce6
ls <936018838@qq.com>
2020-03-16 02:30:07 +0800

    首次提交

--- ts.py
+++ ts.py
@@ -50,51 +50,51 @@ def download(url, n):
 
 
 if __name__ == '__main__':
-    # print("----------Init Mid File...-------------")
-    # fps = []
-    # os.system("rm -r {}*.ts".format(TS_FILE))
-    # os.system("rm -r {}*.txt".format(TS_FILE))
-    # time.sleep(1)
-    # po = Pool(100)
-    # for i in range(BEGIN, END):
-    #     fps.append(i)
-    #     distance = L - len(str(i))
-    #     n = "{}{}".format("0" * distance, i) if distance != 0 else i
-    #     url = "{}{}.ts".format(URL, n)
-    #     po.apply_async(download, args=(url, n))
-    # po.close()
-    # po.join()
-    # print("Over File DownLoad...")
-    #
-    # time.sleep(1)
-    # file_list = sorted(os.listdir(TS_FILE))[1:]
-    # ts_group = []
-    # for file in file_list:
-    #     ts_group.append(int(to_num(file.split(".")[0])))
-    #
-    # # 复验下载文件是否完整
-    # print("--------------Check File...------------------")
-    # fill_group = list(set(fps) - set(ts_group))
-    # while fill_group:
-    #     print("-----Total{}Need to be supplemented------".format(len(fill_group)))
-    #     po = Pool(100)
-    #     for j in fill_group:
-    #         distance = L - len(str(j))
-    #         n = "{}{}".format("0" * distance, j) if distance != 0 else j
-    #         url = "{}{}.ts".format(URL, n)
-    #         po.apply_async(download, args=(url, n))
-    #     po.close()
-    #     po.join()
-    #     file_list = sorted(os.listdir(TS_FILE))[1:]
-    #     for file in file_list:
-    #         ts_group.append(int(to_num(file.split(".")[0])))
-    #     fill_group = list(set(fps) - set(ts_group))
-    # # 生成ffmpeg所需文件目录
-    # print("----------------Build File List...-----------------")
-    # with open("{}file_list.txt".format(TS_FILE), "w+") as f:
-    #     for file in file_list:
-    #         f.write("file '{}'\n".format(file))
-    #
-    # print("----------------Merge Files...-----------------")
-    # os.system("ffmpeg -f concat -i {}file_list.txt -c copy {}{}.mp4".format(TS_FILE, MP4_FILE, NAME))
-    # print("----------Download Success---------")
+    print("----------Init Mid File...-------------")
+    fps = []
+    os.system("rm -r {}*.ts".format(TS_FILE))
+    os.system("rm -r {}*.txt".format(TS_FILE))
+    time.sleep(1)
+    po = Pool(100)
+    for i in range(BEGIN, END):
+        fps.append(i)
+        distance = L - len(str(i))
+        n = "{}{}".format("0" * distance, i) if distance != 0 else i
+        url = "{}{}.ts".format(URL, n)
+        po.apply_async(download, args=(url, n))
+    po.close()
+    po.join()
+    print("Over File DownLoad...")
+
+    time.sleep(1)
+    file_list = sorted(os.listdir(TS_FILE))[1:]
+    ts_group = []
+    for file in file_list:
+        ts_group.append(int(to_num(file.split(".")[0])))
+
+    # 复验下载文件是否完整
+    print("--------------Check File...------------------")
+    fill_group = list(set(fps) - set(ts_group))
+    while fill_group:
+        print("-----Total{}Need to be supplemented------".format(len(fill_group)))
+        po = Pool(100)
+        for j in fill_group:
+            distance = L - len(str(j))
+            n = "{}{}".format("0" * distance, j) if distance != 0 else j
+            url = "{}{}.ts".format(URL, n)
+            po.apply_async(download, args=(url, n))
+        po.close()
+        po.join()
+        file_list = sorted(os.listdir(TS_FILE))[1:]
+        for file in file_list:
+            ts_group.append(int(to_num(file.split(".")[0])))
+        fill_group = list(set(fps) - set(ts_group))
+    # 生成ffmpeg所需文件目录
+    print("----------------Build File List...-----------------")
+    with open("{}file_list.txt".format(TS_FILE), "w+") as f:
+        for file in file_list:
+            f.write("file '{}'\n".format(file))
+
+    print("----------------Merge Files...-----------------")
+    os.system("ffmpeg -f concat -i {}file_list.txt -c copy {}{}.mp4".format(TS_FILE, MP4_FILE, NAME))
+    print("----------Download Success---------")
